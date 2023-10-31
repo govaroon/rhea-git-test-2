1c19ea145157323b9fa61253ca176debb546a0e1
Renatha Putri <urneuross@gmail.com>
2023-10-31 23:28:21 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus quasi debitis vero molestiae illum veritatis! Cum asperiores placeat debitis pariatur maxime, dolore unde magnam, quisquam nemo nesciunt sed ex quibusdam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
